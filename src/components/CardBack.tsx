560402723cd221903828bc2451748862e6946d15
Vladimir Aleksic <vlada.pzs@hotmail.com>
2023-01-21 17:24:15 +0100

    Card front

--- src/components/CardBack.tsx
+++ src/components/CardBack.tsx
@@ -2,7 +2,7 @@ const CardBack = () => {
   return (
     <div className='flex flex-col items-center absolute bottom-1/4 right-[-200px] w-[400px] h-[228px] bg-gradient-to-br shadow-xl from-white to-gray-300 z-50 rounded-[9px]'>
       <div className='bg-zinc-800 w-full h-12 mt-6'></div>
-      <div className='bg-gray-400 w-4/5 h-8 rounded-sm mt-6 text-white flex items-center justify-end px-4 text-sm tracking-widest'>
+      <div className='bg-gray-400 w-4/5 h-8 rounded-sm mt-6 text-gray-50 flex items-center justify-end px-4 text-sm tracking-widest font-medium'>
         <p>000</p>
       </div>
       <div className='mt-3'>
--- src/components/CardFront.tsx
+++ src/components/CardFront.tsx
@@ -1,5 +1,19 @@
 const CardFront = () => {
-  return <div className='absolute top-1/3 right-0'>CardFront</div>
+  return (
+    <div className='flex flex-col items-center absolute top-1/4 right-[-100px] w-[400px] h-[228px] bg-gradient-to-br shadow-xl from-purple-700 to-purple-800 z-50 rounded-[9px]'>
+      <div className='flex items-center w-full gap-4 justify-start px-8 pt-8'>
+        <div className='w-10 h-10 rounded-full bg-gray-50'></div>
+        <div className='w-5 h-5 rounded-full border-gray-50 border-2'></div>
+      </div>
+      <div className='w-fit font-medium text-2xl text-gray-50 tracking-[0.24em] mt-16 mr-[-0.24em]'>
+        <p>0000 0000 0000 0000</p>
+      </div>
+      <div className='flex w-full justify-between px-8 mt-5 text-gray-50 text-sm tracking-widest'>
+        <p>JANE APPLEASEED</p>
+        <p>00/00</p>
+      </div>
+    </div>
+  )
 }
 
 export default CardFront
